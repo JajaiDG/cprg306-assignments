9c292d8e754a952ac5e893058d0bf4f2ee56d32e
JajaiDG <Jairo.DeGuzman@edu.sait.ca>
2024-01-29 18:41:30 -0700

    commit to master

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+// pages/week-2/student-info.js
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Jairo De Guzman</p>
+      <Link href={"https://github.com/JajaiDG/cprg306-assignments"}>Github Repository
+      </Link>
+    </div>
+  );
+}
