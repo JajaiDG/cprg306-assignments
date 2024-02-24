5c5d453ec46ff13e851206be51197d516cd05f7d
JajaiDG <Jairo.DeGuzman@edu.sait.ca>
2024-02-24 16:18:35 -0700

    commit to master
    week-5 assignment

--- app/page.js
+++ app/page.js
@@ -22,6 +22,11 @@ export default function Home() {
             Week 4
           </div>
         </Link>
+        <Link href="/week-5">
+          <div className="text-blue-500 hover:text-blue-700 transition duration-300">
+            Week 5
+          </div>
+        </Link>
       </div>
       <div className="text-5xl text-gray-900 border-0 items-center">
       </div>
